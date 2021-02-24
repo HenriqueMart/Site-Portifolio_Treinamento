d66cd3c7db1498845968da7962171d134a9f3c35
HenriqueMart <h.martins.silva2014@gmail.com>
2021-02-24 15:09:30 -0300

    atualização

--- Portifolio/script.js
+++ Portifolio/script.js
@@ -1,3 +1,27 @@
 document.querySelector(".hanburguer").addEventListener("click", () =>
     document.querySelector(".container").classList.toggle("show-menu")
 );
+document.querySelector("#qtde").addEventListener("change", atualizarPreco)
+document.querySelector("#js").addEventListener("change", atualizarPreco)
+document.querySelector("#layout-sin").addEventListener("change", atualizarPreco)
+document.querySelector("#layout-nao").addEventListener("change", atualizarPreco)
+document.querySelector("#prazo").addEventListener("change", function () {
+    const prazo = document.querySelector("#prazo").value
+    document.querySelector("label[for=prazo]").innerHTML = `Prazo: ${prazo} Semanas`
+    atualizarPreco()
+})
+
+
+function atualizarPreco(){
+    const qtde = document.querySelector("#qtde").value
+    const temJS = document.querySelector("#js").checked
+    const incluilayout = document.querySelector("#layout-sin").checked
+    const prazo = document.querySelector("#prazo").value
+
+    let preco = qtde * 100;
+    if (temJS) preco *= 1.1
+    if(incluilayout) preco += 500
+    let taxaurgencia = 1 - prazo*0.1;
+    preco *= 1 + taxaurgencia
+    document.querySelector("#preco").innerHTML = `R$ ${preco.toFixed(2)}`
+}
